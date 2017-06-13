3eb82684ac96f4c541256c31bf9db6dc6b3cd95e
MKD <mokundong92@gmail.com>
2017-06-13 20:21:40 +0800

    MKD

--- pycode/untitled0.py
+++ pycode/untitled0.py
@@ -7,8 +7,11 @@ Created on Mon Jun 12 21:42:55 2017
 
 import pandas as pd
 import numpy as np
-res=pd.read_csv('res.csv')
 import re
+
+res=pd.read_csv('res.csv')
+df=pd.read_csv('df.csv')
+df_A = df[(df['target']=='A')]
 
 rules = res.ix[:,1]
 new_rules = []
@@ -21,18 +24,44 @@ for tup in rules:
     tmp.sort()
     tmp = "".join([str(i) for i in tmp])
     new_rules.append(tmp)
-dic = dict(zip(res.ix[:,0],new_rules))
+dic = dict(zip((res.ix[:,0]),new_rules))
 rev_dic = {}
 for k,v in dic.items():
     rev_dic.setdefault(v, []).append(k)
     
     
 ###########################################
-df=pd.read_csv('df.csv')
-target_col = df.ix[:,16]
-A=B=0
-for col in target_col:
-    if col == 'A':
-        A = A + 1
-    else:
-        B = B + 1
+
+m=[]
+for k,v in rev_dic.items():
+    l=[]
+    for j in v:
+        ex = rules[j-1]
+        invert_1 = []
+        invert_1 = re.compile(reExp1).findall(ex)
+        tmpe = list(map(lambda x :(int(x[0]),int(x[1])),invert_1))
+        TL = 74823*[True]
+        IN = []
+        for ii,jj in tmpe:
+            a = df_A.ix[:,ii] == jj
+            TL = TL & a
+            A = sum(TL)
+            inter = (10000*A - A*A)/(74823*25177)
+            IN.append(inter)
+        
+        break
+            
+#            n.append(h)
+#            for i,j in n:
+#                print(i,j)
+#        break
+         
+#            TL = 74823*[True]
+#            IN = []
+#            for i,j in h:
+#                print(i,j)
+#                a = df_A.ix[:,i] == j
+#                TL = TL & a
+#                A = sum(TL)
+#                interest = (10000*A - A*A)/(74823*25177) 
+#                IN.append(interest)
